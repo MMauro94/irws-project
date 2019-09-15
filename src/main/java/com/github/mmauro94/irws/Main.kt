b11818d46c5c308e4caf492ce93c6812240415ba
Mauro Molin <molin.mauro@femastudios.com>
2019-09-15 21:14:10 +0200

    Added runs

--- src/main/java/com/github/mmauro94/irws/Main.kt
+++ src/main/java/com/github/mmauro94/irws/Main.kt
@@ -7,16 +7,16 @@ package com.github.mmauro94.irws
 /**
  * Limits the number of documents to parse. For debug only. Use [Integer.MAX_VALUE] to have no limits.
  */
-const val MAX_DOCUMENTS = 10000
+const val MAX_DOCUMENTS = Integer.MAX_VALUE
 
 /**
  * The radius parameter to pass to [streamCluster]
  */
-const val STREAM_CLUSTER_RADIUS = 0.925
+const val STREAM_CLUSTER_RADIUS = 0.9
 /**
  * The maximum number of clusters
  */
-const val MAX_CLUSTERS = 2000
+const val MAX_CLUSTERS = 1500
 
 /**
  * All the encodings to use in the calculation of D-Gaps
@@ -32,7 +32,7 @@ val ENCODINGS = listOf(
  * Entry point
  */
 fun main() {
-    //Point 1: Parse documents and transform them in a set of term IDs
+    //Step 1: Parse documents and transform them in a set of term IDs
     print("Reading documents...")
     val documents = documents().take(MAX_DOCUMENTS).toList()
     println("OK")
@@ -45,19 +45,19 @@ fun main() {
     initialDGaps.print()
     println()
 
-    //Point 2: sort the collection by document length, and run the stream cluster algorithm
+    //Step 2: sort the collection by document length, and run the stream cluster algorithm
     println("Computing clusters...")
     val clusters = documents.sortedByDescending { it.terms.size }.streamCluster(STREAM_CLUSTER_RADIUS, MAX_CLUSTERS)
     println("Clustered completed. Obtained ${clusters.size} clusters")
     println()
 
-    //Point 3 and 4: run TSP and remap documents IDs using the TSP-induced order
+    //Step 3 and 4: run TSP and remap documents IDs using the TSP-induced order
     print("Remapping document IDs using TSP...")
     val remappedDocuments = clusters.runTSP().remap()
     println("OK")
     println()
 
-    //Point 5: compute and print D-Gaps of remapped documents, comparing with initial D-Gaps
+    //Step 5: compute and print D-Gaps of remapped documents, comparing with initial D-Gaps
     println("---- AFTER TSP D-GAPS ----")
     remappedDocuments.computeDGaps().print(initialDGaps)
 }
